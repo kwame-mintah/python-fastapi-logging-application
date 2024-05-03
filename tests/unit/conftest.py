1eec645d5c614d907d2325ee5d635f93c2ceadc7
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-05-03 21:58:10 +0100

    test(conftest): interpolate filepath differently using `os.path.join()`
    attempt to resolve pytest runs in github workflow

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -10,6 +10,7 @@ def set_test_pickle_file_location(monkeypatch):
     """
     Override file path, to load `test_data.pkl` file instead.
     """
+    directory_path = os.path.dirname(os.path.realpath(__file__))
     monkeypatch.setattr(
-        demo_service, "PICKLE_FILENAME", os.path.realpath("test_data.pkl")
+        demo_service, "PICKLE_FILENAME", os.path.join(directory_path, "test_data.pkl")
     )
