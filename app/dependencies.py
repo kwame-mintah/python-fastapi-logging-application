0e2b4a8b79cd067c24a0ac9f27192491ded8e9e9
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-05-03 21:11:04 +0100

    docs(dget_demo_service): include docstring for function
    provide context that function is calling a stubbing / mocking service.

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,4 +2,8 @@ from app.services.demo_service import DemoService
 
 
 def get_demo_service():
+    """
+    Service to provide stubbing / mocking for
+    demonstrating API responses.
+    """
     return DemoService
