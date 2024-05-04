cab16bd94e719c467e9c7c35b13acb5a6d1fe7b4
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-05-04 22:22:14 +0100

    test(demo_service): additional tests for http exceptions raised
    additional coverage for functions that throw http exceptions, asserting that the correct http status code is returned. use `monkeypatch` to override constants to cause errors within the code

--- tests/unit/test_demo_service.py
+++ tests/unit/test_demo_service.py
@@ -1,4 +1,8 @@
+import pytest
+from fastapi import HTTPException, status
+
 from app.models.event_models import EventLog, UserEvent, InsertResult
+from app.services import demo_service
 from app.services.demo_service import DemoService
 
 
@@ -6,6 +10,13 @@ def test_should_return_event_logs() -> None:
     service = DemoService()
     result = service.return_event_logs(size=1)
     assert len(result) == 1
+
+
+def test_return_event_logs_raises_http_500_exception(monkeypatch) -> None:
+    monkeypatch.setattr(demo_service, "PICKLE_FILENAME", "invalid")
+    with pytest.raises(HTTPException) as e:
+        demo_service.DemoService().example_return_event_logs(size=1)
+    assert e.value.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
 def test_should_return_single_event_log() -> None:
@@ -19,6 +30,19 @@ def test_should_return_single_event_log() -> None:
             username="my_user", email="my_user@email.com", operation="read"
         ),
     )
+
+
+def test_return_event_log_raises_http_500_exception(monkeypatch) -> None:
+    monkeypatch.setattr(demo_service, "PICKLE_FILENAME", "invalid")
+    with pytest.raises(HTTPException) as e:
+        demo_service.DemoService().example_return_event_log(event_id="u_001")
+    assert e.value.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+
+
+def test_return_event_log_raises_http_404_exception() -> None:
+    with pytest.raises(HTTPException) as e:
+        demo_service.DemoService().example_return_event_log(event_id="u_007")
+    assert e.value.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_should_insert_valid_event_log() -> None:
@@ -49,3 +73,28 @@ def test_should_not_insert_invalid_event_log() -> None:
     assert result == [
         InsertResult(event_id="s_123", success=False, error="invalid_location")
     ]
+
+
+def test_insert_event_logs_raises_http_400_exception(monkeypatch) -> None:
+    example_event_log = [
+        {
+            "type": "system",
+            "timestamp": "2006-01-13T00:00:00Z",
+            "event_id": "s_123",
+            "event": {"system_id": "id_123", "location": "jetix", "operation": "read"},
+        },
+        {
+            "type": "user",
+            "timestamp": "2023-10-01T13:45:00.000Z",
+            "event_id": "u_123",
+            "event": {
+                "username": "my_user",
+                "email": "my_user@email.com",
+                "operation": "write",
+            },
+        },
+    ]
+    monkeypatch.setattr(demo_service, "MAX_SIZE", 1)
+    with pytest.raises(HTTPException) as e:
+        demo_service.DemoService().insert_event_logs(event_logs=example_event_log)
+    assert e.value.status_code == status.HTTP_400_BAD_REQUEST
